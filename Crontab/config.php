f71d222681bc51f5e4d9e7ddc3dcb984c92ba67f
charles <gaodihu@126.com>
2016-03-17 17:51:43 +0800

    fix bug

--- Crontab/config.php
+++ Crontab/config.php
@@ -1,14 +1,23 @@
 <?php
 //网站数据库数据
+
+$dir = str_replace("\\",'/',__DIR__);
+$dir = substr($dir,0,0 - strlen('Crontab'));
+
+date_default_timezone_set('Asia/Chongqing');
+
 $host =array(
-    'host'=>'127.0.0.1:7709',
-    'user'=>'myled',
-    'password'=>'232ZFit52brxaN4n'
+    'host'=>'127.0.0.1',
+    'user'=>'root',
+    'password'=>'',
+    'dbname' => 'moresku',
 );
 
-$db_connect_magento=mysql_connect($host['host'],$host['user'],$host['password']) or die("Unable to connect to the mageto MySQL!");
-//$db_connect_opencart=mysql_connect($opencart_db['host'],$opencart_db['user'],$opencart_db['password'],true) or die("Unable to connect to the opencart MySQL!");
-mysql_query("SET NAMES UTF8");
-mysql_select_db("new_myled",$db_connect_magento);
-date_default_timezone_set('Asia/Chongqing');
+$db = mysqli_connect($host['host'],$host['user'],$host['password'],$host['dbname']) or die("Unable to connect to the mageto MySQL!");
+
+mysqli_query($db,"SET NAMES UTF8");
+
+mysqli_select_db($db,"new_myled");
+
+
 ?>
